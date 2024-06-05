313dd6b6a352066b4ae6c28f6e13cdf2f8b386fc
Symbat Arstanbekova <symbatarstanbek@gmail.com>
2024-06-05 13:29:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import YellowWorld from "./YellowWorld";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <YellowWorld></YellowWorld>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/YellowWorld.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const YellowWorld = () => {
+  return <div>Yellow World!</div>;
+};
+
+export default YellowWorld;
